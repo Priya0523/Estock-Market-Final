11a7b5da214427420a2e573a99e0e33098a554b2
unknown <cogjava525@iiht.tech>
2022-10-13 15:16:46 +0530

    both frond end and Backend changes for stock market

--- stockService/src/main/java/com/example/fes/stockService/stockService/Controller/StockController.java
+++
@@ -1,4 +0,0 @@
-package com.example.fes.stockService.stockService.Controller;
-
-public class StockController {
-}
--- stockService/src/main/java/com/example/fes/stockService/stockService/Controller/StockPriceController.java
+++ stockService/src/main/java/com/example/fes/stockService/stockService/Controller/StockPriceController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1.0/market/stock")
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200/")
 public class StockPriceController {
 	
 	Logger logger = LoggerFactory.getLogger(StockPriceController.class); 
